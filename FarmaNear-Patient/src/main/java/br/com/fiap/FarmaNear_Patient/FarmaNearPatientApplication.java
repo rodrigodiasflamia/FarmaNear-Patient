282440392dbc2581d383f3d2d99422674e0bc46e
rodrigodiasflamia <rodrigo.flamia@unoesc.edu.br>
2025-06-20 10:49:12 -0300

    Initial commit

---
+++ FarmaNear-Patient/src/main/java/br/com/fiap/FarmaNear_Patient/FarmaNearPatientApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.FarmaNear_Patient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmaNearPatientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmaNearPatientApplication.class, args);
+	}
+
+}
---
+++ FarmaNear-Patient/src/test/java/br/com/fiap/FarmaNear_Patient/FarmaNearPatientApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.FarmaNear_Patient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmaNearPatientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
